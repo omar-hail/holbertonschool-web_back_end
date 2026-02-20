1ea3a8888e7472cb45a410babb7cce6f4ea66ff1
omar-hail <donz.h.h@gmail.com>
2026-02-20 19:07:09 +0000

    Create floor function

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module for getting the floor of a float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+
+    Args:
+        n: Float number to floor
+
+    Returns:
+        The floor of n as an integer
+    """
+    return int(n)
