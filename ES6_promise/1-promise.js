600d19196cfbafd6ef1932c35ab3ba0dae2541e0
omar-hail <donz.h.h@gmail.com>
2026-04-04 20:43:24 +0000

    tasks ES6_promise

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+      return;
+    }
+
+    reject(new Error('The fake API is not working currently'));
+  });
+}
